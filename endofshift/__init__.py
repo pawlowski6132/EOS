841a5502fa2b54317256c2723f156dc8427f0b2e
Joe Pawlowski <j.pawlowski6132@gmail.com>
2020-12-27 09:28:39 -0500

    12-27-20

--- endofshift/__init__.py
+++ endofshift/__init__.py
@@ -1,2 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
+
+from endofshift.core.views import core
+from endofshift.error_pages.handlers import error_pages
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
+
